4dc80d6ebfc07e29cd04310a800ee0c1e646cbaf
Thies Verhave <thies@verhave.net>
2024-12-31 19:48:24 +0100

    Add unit test

---
+++ tests/Unit/ImageExtractorTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GameboyCameraImageExtractor\Tests;
+
+use GameboyCameraImageExtractor\ImageExtractor;
+use PHPUnit\Framework\TestCase;
+use function base64_encode;
+use function imagetruecolortopalette;
+use function ob_get_contents;
+use function ob_start;
+use function sprintf;
+use function var_dump;
+
+final class ImageExtractorTest extends TestCase
+{
+    private readonly ImageExtractor $imageExtractor;
+
+    public function setUp() : void
+    {
+        $this->imageExtractor = new ImageExtractor();
+    }
+
+    public function testGetImages() : void
+    {
+        $saveData = $this->imageExtractor->unpackSaveData(file_get_contents(__DIR__ . '/../Fixtures/test.sav'));
+        $images = $this->imageExtractor->getImages($saveData);
+
+        foreach ($images as $key => $image) {
+            $testImagePath = sprintf('%s/../Fixtures/images/png/output_%d.png', __DIR__, ($key + 1));
+
+            imagetruecolortopalette($image, true, 4);
+
+            ob_start();
+            imagepng($image);
+            $imageData = ob_get_contents();
+            ob_end_clean();
+
+            $this->assertEquals(base64_encode($imageData), base64_encode(file_get_contents($testImagePath)));
+        }
+    }
+}
